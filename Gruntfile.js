c4801696b3576f410fdad41fda650a36eb1ae917
Afonso F <afonso.franca@gmail.com>
2015-01-21 21:42:59 -0200

    Prototype

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    ejs:{
+      test:{
+        src: 'templates/resume.ejs',
+        options: require('./data/resume.json'),
+        dest: 'dist/resume.html'
+      }
+    }
+  });
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Default task(s).
+  grunt.registerTask('default', ['ejs']);
+
+};
