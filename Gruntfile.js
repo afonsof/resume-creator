9dad383400ba06e4e3bc8fa348872f6b4dd63931
Afonso F <afonsof@Osx-Espeon.local>
2015-03-23 18:45:10 -0300

    SCSS and HTML5

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,45 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    ejs:{
-      test:{
-        src: 'templates/resume.ejs',
-        options: require('./data/resume.json'),
-        dest: 'dist/resume.html'
-      }
-    },
-    copy: {
-      main:{
-        files: [
-          {expand: true, cwd: 'templates/', src: ['**'], dest: 'dist/'},
-        ]
-      }
-    },
-    clean: {
-      main: ["dist/*.ejs"],
-    },
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ejs: {
+            test: {
+                src: 'templates/resume.ejs',
+                options: require('./data/resume.json'),
+                dest: 'dist/resume.html'
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'templates/', src: ['**'], dest: 'dist/'}
+                ]
+            }
+        },
+        clean: {
+            main: ["dist/*.ejs", "dist/*.scss"]
+        },
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {
+                    'dist/resume.css': 'templates/resume.scss'
+                }
+            }
+        }
+    });
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-  // Default task(s).
-  grunt.registerTask('default', ['ejs', 'copy', 'clean']);
+    // Default task(s).
+    grunt.registerTask('default', ['ejs', 'sass', 'copy', 'clean']);
 
 };
