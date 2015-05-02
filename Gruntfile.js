dc17b06543a747afc51c3979b93822a6e11788ed
Afonso F <afonso.franca@gmail.com>
2015-05-01 22:36:46 -0300

    Commit css

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,6 +50,6 @@ module.exports = function (grunt) {
 
     // Default task(s).
     grunt.registerTask('default', ['ejs', 'sass:prototype', 'copy', 'clean']);
-    grunt.registerTask('heroku', ['sass:dist']);
+    grunt.registerTask('heroku', []);
 
 };
